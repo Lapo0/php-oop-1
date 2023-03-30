f73df9d7fad6ef40c64b52047eb411df233330b0
Lapo0 <lapo.gini.01@gmail.com>
2023-03-30 15:11:22 +0200

    FEAT istanze

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 // creo una classe movie
 class Movie {
-    
+
+    // creare variabili d?istanza
+    public string $titol;
+    public string $description;
+    public string $actor;
+    public int $star;
 }
 
 
