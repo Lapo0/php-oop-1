5f368a6b4769e49ae8bdebca4abe15e8cb6f02fc
Lapo0 <lapo.gini.01@gmail.com>
2023-03-30 15:41:05 +0200

    FEAT seconda istanza

--- index.php
+++ index.php
@@ -17,9 +17,13 @@ class Movie {
 }
 
 // ISTANZIO LA CLASSE MOVIE
-$film1 = new Movie("Bitcoin", 4);
+$film1 = new Movie("Bitcoin", 5);
 
 var_dump($film1);
+
+$film2 = new Movie("Bernard Madoff", 0);
+
+var_dump($film2);
 
 
 ?>
@@ -31,8 +35,19 @@ var_dump($film1);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
